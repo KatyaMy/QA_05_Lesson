11674f71066a2481c304ed88ef13119d0ef0ef19
KatyaMy <20FamGam20@gmail.com>
2022-09-23 23:44:04 -0400

    hw1-3

---
+++ HomeWork/HW_1.py
@@ -0,0 +1,20 @@
+# Решение
+# 1.1
+# .2.
+# print('Hello world!')
+#
+# user_name = input()
+# print(f'Hello, {user_name}!')
+
+# 1.2.Способ
+# 1:
+print(' ** ** ** ** * ')
+print(' * * ')
+print(' * * ')
+print(' ** ** ** ** * ')
+
+# Способ
+# 2:
+print('*********\n*       *\n*       *\n*********')
+
+
