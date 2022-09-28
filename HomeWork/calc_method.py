170e7cf21c7a004aaa0e7d4ca448cac511c5a03b
KatyaMy <20FamGam20@gmail.com>
2022-09-28 14:28:48 -0400

    fix HM and Lesson 4-5

--- HomeWork/calc_method.py
+++ HomeWork/calc_method.py
@@ -12,8 +12,9 @@ def tests():
     assert add(2, 3) == 5, f'Wrong number, actual result is {add(5, 8)}'
     assert minus(113, 33) == 80, f'Wrong number, actual result is {minus(113, 33)}'
     assert multi(23, 9) == 207, f'Wrong number, actual result is {multi(23, 9)}'
-    assert devision(45, 7) == 6.4, f'Wrong number, actual result is {devision(45, 7)}'
+    assert devision(45, 7) == 6.428571428571429, f'Wrong number, actual result is {devision(45, 7)}'
     assert exp(23, 4) == 279841, f'Wrong number, actual result is {exp(23, 4)}'
+    assert r_of_dev(33, 2) == 0, f'Wrong number, actual result is {exp(23, 4)}'
 
 
 tests()
