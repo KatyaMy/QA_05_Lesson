4bdfbd9d97eaef4f516d8b7640e7cc4911e9306a
KatyaMy <20FamGam20@gmail.com>
2022-09-28 14:14:06 -0400

    fix HM and Lesson 4-5

--- HomeWork/calc_method.py
+++ HomeWork/calc_method.py
@@ -8,11 +8,11 @@ print(f'Total: {exp(23, 4)}')
 print(f'Total: {r_of_dev(33, 2)}')
 
 
-# def tests():
-#     assert add(5, 8) == 13, f'Wrong number, actual result is {add(5, 8)}'
-#     assert minus(10, 6) == 60, f'Wrong number, actual result is {minus(113, 33)}'
-#     assert multi(10, 0) == "Can't divide by zero", "Shouldn't divide by zero"
-#     assert devision(85, 28) == 57, f'Wrong number, actual result is {multi(85, 28)}'
-#     assert exp(9, 4) == 1, f'Wrong number, actual result is '
-#
-# tests()
+def tests():
+    assert add(5, 8) == 13, f'Wrong number, actual result is {add(5, 8)}'
+    assert minus(10, 6) == 60, f'Wrong number, actual result is {minus(113, 33)}'
+    assert multi(10, 0) == "Can't divide by zero", "Shouldn't divide by zero"
+    assert devision(85, 28) == 57, f'Wrong number, actual result is {multi(85, 28)}'
+    assert exp(9, 4) == 1, f'Wrong number, actual result is '
+
+tests()
